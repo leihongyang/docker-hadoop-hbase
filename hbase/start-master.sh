6ca8217551e9198959d4fadf81c12269c966b8f7
lhy <136678037@qq.com>
2018-09-12 17:10:16 +0800

    auto hosts

--- hbase/start-master.sh
+++ hbase/start-master.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 docker run -itd -p 16010:16010 --name hbase-master --hostname hbase-master kycloud/hbase:1.0.0 &> /dev/null
+#hadoopMasterIP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' hadoop-master`
+#docker exec -it hbase-master sed '2i'${hadoopMasterIP}'     hadoop-master' /etc/hosts
