65a5da41a39b378d59e4506370fb44c807ed27f1
lhy <136678037@qq.com>
2018-09-18 08:56:53 +0000

    fix

--- hadoop/config/start-hadoop.sh
+++ hadoop/config/start-hadoop.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 service ssh start
+
+$HADOOP_HOME/sbin/stop-dfs.sh
+
+service ssh restart
+
 echo -e "\n"
 
 $HADOOP_HOME/sbin/start-dfs.sh
 
 echo -e "\n"
 
-tailf $HADOOP_HOME/logs/hadoop-root-namenode-hadoop-master.log
+#tailf $HADOOP_HOME/logs/hadoop-root-namenode-hadoop-master.log
+tailf $HADOOP_HOME/logs/hadoop-root-*
 #$HADOOP_HOME/sbin/start-yarn.sh
 
 #echo -e "\n"
